9c3851f3ead7bcd06ead80e727c077e73e14fdf5
josdejong <wjosdejong@gmail.com>
2014-01-09 16:09:26 +0100

    Fixed example guess the number

--- examples/guess_the_number.js
+++ examples/guess_the_number.js
@@ -7,7 +7,19 @@ var MIN = 0,
 
 var emma = babble.babbler('emma').subscribe();
 
-var checkGuess = babble.decide(function (guess, context) {
+var startGame = function (response, context) {
+  // choose a random value
+  context.number = randomInt(MIN, MAX);
+
+  console.log('emma: ok I have a number in mind between ' + MIN + ' and ' + MAX);
+  return 'ok';
+};
+
+var denyGame = function () {
+  return 'no thanks';
+};
+
+function check (guess, context) {
   if (guess < context.number) {
     return 'higher';
   }
@@ -17,64 +29,45 @@ var checkGuess = babble.decide(function (guess, context) {
   else  {
     return 'right';
   }
-}, {
-  higher: babble.reply(function () {
-        console.log('emma: higher');
-        return 'higher';
-      })
-      .reply(checkGuess), // TODO: this doesn't work
-  lower: babble.reply(function () {
-        console.log('emma: lower');
-        return 'lower';
-      })
-      .reply(checkGuess), // TODO: this doesn't work
-  right: babble.reply(function () {
-        console.log('emma: right!');
-        return 'right';
-      })
-});
+}
 
-var startGame = babble.reply(function (response, context) {
-  // choose a random value
-  context.number = randomInt(MIN, MAX);
+var options = {};
+var validateGuess = babble.decide(check, options).done();
+options.higher = babble.reply(function () {
+      console.log('emma: higher');
+      return 'higher';
+    })
+    .then(validateGuess)
+    .done();
 
-  console.log('emma: ok I have a number in mind between ' + MIN + ' and ' + MAX);
-  return 'ok';
-}, checkGuess);
+options.lower = babble.reply(function () {
+      console.log('emma: lower');
+      return 'lower';
+    })
+    .then(validateGuess)
+    .done();
 
-var denyGame = reply(function () {
-  return 'no thanks';
-});
+options.right = babble.reply(function () {
+      console.log('emma: right!');
+      return 'right';
+    })
+    .done();
 
-emma.listen('lets play guess the number', decide(function () {
-  if (Math.random() > 0.2) {
-    return startGame;
-  }
-  else {
-    return denyGame;
-  }
-}));
+emma.listen('lets play guess the number')
+    .decide(function () {
+      return (Math.random() > 0.2) ? 'start': 'deny';
+    }, {
+      start: babble.reply(startGame)
+          .then(validateGuess)
+          .done(),
+      deny: babble.reply(denyGame)
+          .done()
+    })
+    .done();
 
 /* -------------------------------------------------------------------------- */
 
 var jack = babble.babbler('jack').subscribe();
-
-var triumph = function (response, context) {
-  console.log('jack: I found it! The correct number is: ' + context.number);
-};
-
-var guess = function (response, context) {
-  if (response == 'higher') {
-    context.lower = context.number + 1;
-  }
-  else if (response == 'lower') {
-    context.upper = context.number - 1;
-  }
-
-  context.number = randomInt(context.lower, context.upper);
-  console.log('jack: guessing ' + context.number + '...');
-  return context.number;
-};
 
 var initialize = function (response, context) {
   context.lower = MIN;
@@ -89,23 +82,40 @@ var whine = function () {
   console.log('emma doesn\'t want to play guess the number :(');
 };
 
-var validateGuess = decide(function (response) {
-  if (response == 'right') {
-    return run(triumph);
-  }
-  else {
-    return reply(guess, validateGuess);
-  }
-});
+var triumph = function (response, context) {
+  console.log('jack: I found it! The correct number is: ' + context.number);
+};
 
-jack.ask('emma', 'lets play guess the number', decide(function (response) {
-  if (response == 'ok') {
-    return reply(initialize, validateGuess);
+var guessNext = function (response, context) {
+  if (response == 'higher') {
+    context.lower = context.number + 1;
   }
-  else {
-    return run(whine);
+  else if (response == 'lower') {
+    context.upper = context.number - 1;
   }
-}));
+
+  context.number = randomInt(context.lower, context.upper);
+  console.log('jack: guessing ' + context.number + '...');
+  return context.number;
+};
+
+var guessChoices = {};
+var checkGuess = babble.decide(function (response) {
+  return (response == 'right') ? 'right': 'wrong';
+}, guessChoices).done();
+guessChoices.right = babble.run(triumph).done();
+guessChoices.wrong = babble.reply(guessNext).then(checkGuess).done();
+
+jack.ask('emma', 'lets play guess the number')
+    .decide(function (response) {
+      return (response == 'ok') ? 'ok': 'notOk';
+      }, {
+      ok: babble.reply(initialize)
+          .then(checkGuess)
+          .done(),
+      notOk: babble.run(whine).done()
+    })
+    .done();
 
 
 function randomInt(min, max) {
